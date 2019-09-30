57daea8790d3a640e68e1a9ecf4709dfb73f9546
亂懟 <2511030361@qq.com>
2019-09-30 23:25:09 +0800

    add function defaultToString
    用于将输入转化成字符串

--- JSDataStructureNAlgorithm/util.mjs
+++ JSDataStructureNAlgorithm/util.mjs
@@ -2,3 +2,15 @@
 export function defaultEquals(a, b){
     return a === b;
 }
+
+// 用于将输入转化成字符串
+export function defaultToString(item){
+    if(item === null){
+        return `NULL`;
+    }else if(item === undefined){
+        return `UNDEFINED`;
+    }else if(typeof item === `string` || item instanceof String) {
+        return `${item}`;
+    }
+    return item.toString();
+}
